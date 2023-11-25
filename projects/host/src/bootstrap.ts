a5437359d18a0da94afa6bf1b0bf2482c332f429
Gorbenko_KV <gorbenko_kv@km-union.ru>
2023-11-25 12:18:52 +0300

    host: fixed bootstrap.ts

--- projects/host/src/bootstrap.ts
+++ projects/host/src/bootstrap.ts
@@ -1,13 +1,21 @@
 // @ts-ignore
-import { default as initReactMicroFrontend } from "reactApp/ReactAppInit";
+import( "reactApp/ReactAppInit").then(({default: initMicroFrontend}) => {
+    const reactApp = document.querySelector("#react-app-1");
+
+    if (reactApp) {
+        initMicroFrontend(reactApp);
+    } else {
+        throw Error('react app not found, please check this micro frontend')
+    }
+}).catch((e) => console.log(e))
 
 // @ts-ignore
-import { default as initVueMicroFrontend } from "vueApp/VueAppInit";
+import("vueApp/VueAppInit").then(({default: initMicroFrontend}) => {
+    const vueApp = document.querySelector("#vue-app-1");
 
-const reactApp = document.querySelector("#react-app-1");
-const vueApp = document.querySelector("#vue-app-1");
-
-if (reactApp) {
-    initReactMicroFrontend(reactApp);
-    initVueMicroFrontend(vueApp);
-}
+    if (vueApp) {
+        initMicroFrontend(vueApp);
+    } else {
+        throw Error('vue app not found, please check this micro frontend')
+    }
+}).catch((e) => console.log(e))
